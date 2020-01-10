e797b6c8f006375755771020a63555529d5ed462
hellboyxoxo <50130546+hellboyxoxo@users.noreply.github.com>
2020-01-10 16:19:37 +0530

    Update numb.c
    using pointers

--- numb.c
+++ numb.c
@@ -1,37 +1,34 @@
 #include<stdio.h>
 
-int sum(int,int);
-int sub(int,int);
-int mul(int,int);
-int div(int,int);
+int sum(int *,int *);
+int sub(int *,int *);
+int mul(int *,int *);
+int div(int *,int *);
 
 int main()
 {
 int a,b;
 printf("enter two numbers:");
 scanf("%d%d",&a,&b);
-printf("sum = %d",sum(a,b));
-printf("difference = %d",sub(a,b));
-printf("product = %d",mul(a,b));
-printf("divided value = %d",div(a,b));
+int *p,*q;
+p=&a;
+q=&b;
+printf("sum = %d",sum(p,q));
+printf("difference = %d",sub(p,q));
+printf("product = %d",mul(p,q));
 }
 
-int sum(int p,int q)
+int sum(int *p,int *q)
 {
-return p+q;
+return *p+*q;
 }
 
-int sub(int p,int q)
+int sub(int *p,int *q)
 {
-return p-q;
+return *p-*q;
 }
 
-int mul(int p,int q)
+int mul(int *p,int *q)
 {
-return p*q;
-}
-
-int div(int p,int q)
-{
-return p/q;
+return *p**q;
 }
